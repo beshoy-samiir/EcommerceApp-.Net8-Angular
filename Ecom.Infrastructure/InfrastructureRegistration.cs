2af5a93e1d7d4ae2358e425ccf5e9f6038de12a2
Beshoy Samir <beshoysamir735@gmail.com>
2025-04-04 00:29:54 +0200

    Add project files.

---
+++ Ecom.Infrastructure/InfrastructureRegistration.cs
@@ -0,0 +1,31 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+using Ecom.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure
+{
+    public static class InfrastructureRegistration
+    {
+        public static IServiceCollection infrastructureConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IGenaricRepository<>), typeof(GenaricRepository<>));
+            //services.AddScoped<ICategoryRepository, CategoryRepository>();
+            //services.AddScoped<IProductRepository, ProductRepository>();
+            //services.AddScoped<IPhotoRepository, PhotoRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddDbContext<AppDbContext>(op => 
+            {
+                op.UseSqlServer(configuration.GetConnectionString("EcomDatabase"));
+            });
+            return services;
+        }
+    }
+}
---
+++ Ecom.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Ecom.Core.Entities.Product;
+using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Repositories
+{
+    public class ProductRepository : GenaricRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
